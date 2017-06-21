cf764ea3723a8f613baf3ae0c7dfd83c5504156d
Tabima <caifaz01@gmail.com>
2017-06-20 17:42:59 -0700

    Added TestThat tests (all passed succesfully)

---
+++ tests/testthat/test_HMM.R
@@ -0,0 +1,34 @@
+library(effectR)
+fasta.file <- system.file("extdata", "test_infestans.fasta", package = 'effectR')
+ORF <- seqinr::read.fasta(fasta.file)
+REGEX <- regex.search(sequence = ORF, motif = "RxLR")
+REGEX.seq <- lapply(REGEX, function (x) paste(unlist(x),collapse = ""))
+num.hits <- grep(REGEX.seq, pattern="^\\w{12,60}r\\wlr\\w{6,10}eer", perl = T,ignore.case = T)
+
+mafft.path <- paste0(dirname(system("which mafft", intern = T)), "/")
+hmm.path <- paste0(dirname(system("which hmmsearch", intern = T)), "/")
+candidate.rxlr <- hmm.search(original.seq = fasta.file, regex.seq = REGEX, mafft.path = mafft.path, hmm.path = hmm.path)
+
+context("Generating RxLR candidates from HMM")
+
+test_that("effectR can read FASTA alignment correctly ", {
+  expect_equal(class(ORF), "list")
+  expect_equal(class(ORF[[1]]), "SeqFastadna")
+  expect_equal(length(ORF), 27)
+})
+
+test_that("regex.search returns 17 sequences with RxLR motifs ", {
+  expect_equal(length(REGEX), 17)
+  expect_equal(length(num.hits), 17)
+  expect_equal(num.hits, c(1:17))
+})
+
+test_that("candidate.rxlr returns a list with 3 objects, 17 REGEX, 19 HMM and 19 rows in HMM table ", {
+  expect_equal(length(candidate.rxlr), 3)
+  expect_equal(class(candidate.rxlr), "list")
+  expect_equal(names(candidate.rxlr), c("REGEX","HMM","HMM_Table"))
+  expect_equal(length(candidate.rxlr$REGEX), 17)
+  expect_equal(length(candidate.rxlr$HMM), 19)
+  expect_equal(length(candidate.rxlr$HMM_Table), 21)
+})
+
